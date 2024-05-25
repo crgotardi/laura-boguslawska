5011092ec50a986eb3962a33341a87cb2fd76d63
Cristiano <er.cristiano.gotardi@slbenfica.pt>
2024-05-25 15:26:50 +0100

    setup headings theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -27,5 +27,32 @@ export default {
 		},
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		function ({ addComponents, theme }) {
+			addComponents({
+				'.heading': {
+					color: theme('colors.black'),
+					fontWeight: theme('fontWeight.bold')
+				},
+				'h1': {
+					fontSize: theme('fontSize.5xl'),
+				},
+				'h2': {
+					fontSize: theme('fontSize.4xl'),
+				},
+				'h3': {
+					fontSize: theme('fontSize.3xl'),
+				},
+				'h4': {
+					fontSize: theme('fontSize.2xl'),
+				},
+				'h5': {
+					fontSize: theme('fontSize.xl'),
+				},
+				'h6': {
+					fontSize: theme('fontSize.base'),
+				},
+			});
+		},
+	],
 }
