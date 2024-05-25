1d990527d8e540814f53665d00ee15bc83d905cd
Cristiano <er.cristiano.gotardi@slbenfica.pt>
2024-05-25 14:59:52 +0100

    setup theme colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,9 +3,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			'white': '#FFFFFF',
+			'paper-100': "#FFFAF7",
+			'paper-200': "#F2EDEB",
+			'blue-400': '#046BCD',
+			'blue-200': '#68a6E1',
+			'gray': '#838383',
+			'black': '#333333',
+		},
 		extend: {},
 	},
-	plugins: [
-
-	],
+	plugins: [],
 }
