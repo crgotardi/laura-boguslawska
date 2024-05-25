3a64596ca75a5df1c141d7c999b195e6f0067646
Cristiano <er.cristiano.gotardi@slbenfica.pt>
2024-05-25 14:46:54 +0100

    setup tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    prefetch: true
+  prefetch: true,
+  integrations: [
+    tailwind({
+      nesting: true
+    })
+  ]
 });
