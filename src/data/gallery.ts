bfe825039c51fbd6e187f999087bc839cb461878
Cristiano <er.cristiano.gotardi@slbenfica.pt>
2024-05-31 19:21:33 +0100

    create gallery page

---
+++ src/data/gallery.ts
@@ -0,0 +1,59 @@
+import Art1 from '@assets/art-1.png'
+import Art2 from '@assets/art-2.png'
+import Art3 from '@assets/art-3.png'
+import Art4 from '@assets/art-4.png'
+import Art5 from '@assets/art-5.png'
+import Art6 from '@assets/art-6.png'
+import Art7 from '@assets/art-7.png'
+import Art8 from '@assets/art-8.png'
+import Art9 from '@assets/art-9.png'
+
+const gallery = [
+    {
+        id: 1,
+        alt: '',
+        src: Art1,
+    },
+    {
+        id: 2,
+        alt: '',
+        src: Art2,
+    },
+    {
+        id: 3,
+        alt: '',
+        src: Art3,
+    },
+    {
+        id: 4,
+        alt: '',
+        src: Art4,
+    },
+    {
+        id: 5,
+        alt: '',
+        src: Art5,
+    },
+    {
+        id: 6,
+        alt: '',
+        src: Art6,
+    },
+    {
+        id: 7,
+        alt: '',
+        src: Art7,
+    },
+    {
+        id: 8,
+        alt: '',
+        src: Art8,
+    },
+    {
+        id: 9,
+        alt: '',
+        src: Art9,
+    },
+]
+
+export default gallery
