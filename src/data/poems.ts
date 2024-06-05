26c5aefd04d3b87d9dcbfb526cf29c1767777b55
Cristiano <er.cristiano.gotardi@slbenfica.pt>
2024-06-05 23:10:40 +0100

    create poems page

---
+++ src/data/poems.ts
@@ -0,0 +1,85 @@
+const poems = [
+    {
+        id: 1,
+        title: 'Romantic Prelude (to an unfinished poem)',
+        content: `
+            It is a place of long nights,
+            <br>
+            and falling stars.
+            <br>
+            Everything changes.
+            <br>
+            I stare blankly at the night sky
+            <br>
+            in a black hoodie.
+            <br>
+            Wondering if you think too,
+            <br>
+            if you remember,
+            <br>
+            if you even care...
+            <br>
+            My head is spinning,
+            <br>
+            I am rubbing my eyes,
+            <br>
+            I'm tired.
+        `
+    },
+    {
+        id: 2,
+        title: 'Romantic Prelude (to an unfinished poem)',
+        content: `
+            It is a place of long nights,
+            <br>
+            and falling stars.
+            <br>
+            Everything changes.
+            <br>
+            I stare blankly at the night sky
+            <br>
+            in a black hoodie.
+            <br>
+            Wondering if you think too,
+            <br>
+            if you remember,
+            <br>
+            if you even care...
+            <br>
+            My head is spinning,
+            <br>
+            I am rubbing my eyes,
+            <br>
+            I'm tired.
+        `
+    },
+    {
+        id: 3,
+        title: 'Romantic Prelude (to an unfinished poem)',
+        content: `
+            It is a place of long nights,
+            <br>
+            and falling stars.
+            <br>
+            Everything changes.
+            <br>
+            I stare blankly at the night sky
+            <br>
+            in a black hoodie.
+            <br>
+            Wondering if you think too,
+            <br>
+            if you remember,
+            <br>
+            if you even care...
+            <br>
+            My head is spinning,
+            <br>
+            I am rubbing my eyes,
+            <br>
+            I'm tired.
+        `
+    },
+]
+
+export default poems
