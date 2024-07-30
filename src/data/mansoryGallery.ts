b3677bde515f983a6af68fbfc40a8d3c522f1ea8
Cristiano <er.cristiano.gotardi@slbenfica.pt>
2024-07-30 14:51:07 +0100

    fix mansory gallery

---
+++ src/data/mansoryGallery.ts
@@ -0,0 +1,111 @@
+import Art1 from '@assets/art-1.png'
+import Art2 from '@assets/art-2.png'
+import Art3 from '@assets/art-3.png'
+import Art4 from '@assets/art-4.png'
+import Art5 from '@assets/art-5.png'
+import Art6 from '@assets/art-6.png'
+import Art7 from '@assets/art-7.png'
+import Art8 from '@assets/art-8.png'
+import Art10 from '@assets/art-10.png'
+import Art12 from '@assets/art-12.png'
+
+const mansory = {
+    firstColumn: [
+        {
+            id: 1,
+            alt: '',
+            data: Art1,
+            title: 'Art 1',
+            width: 472,
+            height: 584,
+            material: 'asd',
+        },
+        {
+            id: 6,
+            alt: '',
+            data: Art6,
+            title: 'Art 6',
+            width: 100,
+            height: 70,
+            material: 'asd',
+        },
+        {
+            id: 2,
+            alt: '',
+            data: Art2,
+            title: 'Art 2',
+            width: 1408,
+            height: 1200,
+            material: 'asd',
+        },
+    ],
+    secondColumn: [
+        {
+            id: 3,
+            alt: '',
+            data: Art3,
+            title: 'Art 3',
+            width: 1087,
+            height: 1558,
+            material: 'asd',
+        },
+        {
+            id: 5,
+            alt: '',
+            data: Art5,
+            title: 'Art 5',
+            width: 1270,
+            height: 1600,
+            material: 'asd',
+        },
+        {
+            id: 7,
+            alt: '',
+            data: Art7,
+            title: 'Art 7',
+            width: 1200,
+            height: 1600,
+            material: 'asd',
+        },
+    ],
+    thirdColumn: [
+        {
+            id: 4,
+            alt: '',
+            data: Art4,
+            title: 'Art 4',
+            width: 100,
+            height: 70,
+            material: 'asd',
+        },
+        {
+            id: 8,
+            alt: '',
+            data: Art8,
+            title: 'Art 8',
+            width: 1240,
+            height: 830,
+            material: 'asd',
+        },
+        {
+            id: 10,
+            alt: '',
+            data: Art10,
+            title: 'Art 9',
+            width: 1600,
+            height: 1200,
+            material: 'asd',
+        },
+        {
+            id: 12,
+            alt: '',
+            data: Art12,
+            title: 'Art 9',
+            width: 1600,
+            height: 1200,
+            material: 'asd',
+        },
+    ]
+}
+
+export default mansory
